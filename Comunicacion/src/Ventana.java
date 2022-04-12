47ae4e59805b6672d1c980170834cc3ce5eed876
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-04-12 20:53:23 +0200

    Update Ventana.java

--- Comunicacion/src/Ventana.java
+++ Comunicacion/src/Ventana.java
@@ -1,8 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Ventana extends JFrame {
     public Ventana() {
-        this.setSize(500, 500);
+        Toolkit pantalla = getToolkit();
+        Dimension tamanio = pantalla.getScreenSize();
+        int altura = tamanio.height;
+        int anchura = tamanio.width;
+        setSize(anchura, altura);
+        setLocationRelativeTo(null);
+
+        this.setTitle("ventana 1");
         this.setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
