04d8f7f0e91a923e4f0613462e73e521931cce43
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-05-04 19:33:47 +0200

    Update Main.java

--- Comunicacion/src/Main.java
+++ Comunicacion/src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Ventana v1 = new Ventana();
         Ventana v2=new Ventana();
+        Ventana v3=new Ventana();
         v2.setTitle("Ventana 2");
+        
 
     }
 }
